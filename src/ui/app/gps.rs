dd137147b105f1d8fa8ffbc8c10c9b21a882d8fb
Yuta Suzuki <suzuki.y.ee@m.titech.ac.jp>
2024-07-01 13:55:46 +0900

    goalを設定する機能を追加

--- src/ui/app/gps.rs
+++ src/ui/app/gps.rs
@@ -1,21 +1,23 @@
 use std::f64::consts::PI;
 
-use eframe::egui;
+use eframe::egui::{self, Stroke};
 
 pub struct Gps {
     zoom_level: u8,
     lat: f64,
     lon: f64,
     pos: Option<egui::Vec2>,
+    goal: Option<(f64,f64)>,
 }
 
 impl Gps {
     pub fn new() -> Self {
         Self {
             zoom_level: 10,
-            lat: 35.3_f64,
-            lon: 136.1899_f64,
+            lat: 35.297,
+            lon: 136.178,
             pos: None,
+            goal: None,
         }
     }
 }
@@ -50,7 +52,10 @@ impl super::AppUI for Gps {
                     ui.allocate_painter(ui.available_size_before_wrap(), egui::Sense::drag());
 
                 if let Some(pointer_pos) = response.interact_pointer_pos() {
-                    self.pos = Some((pointer_pos - ui.max_rect().min) / size * 256.0);
+                    let mouse_pos=(pointer_pos - ui.max_rect().min) / size * 256.0;
+                    if mouse_pos.x >= 0.0 && mouse_pos.x <= 256.0 && mouse_pos.y >= 0.0 && mouse_pos.y <= 256.0 {
+                        self.pos = Some(mouse_pos);
+                    }
                 }
 
                 if let Some(pos) = self.pos {
@@ -71,6 +76,25 @@ impl super::AppUI for Gps {
                     5.0,
                     egui::Color32::RED,
                 );
+
+                if let Some((lon,lat)) = self.goal {
+                    let goal_x =
+                        ((2.0_f64.powf(self.zoom_level as f64 + 7.0)) * (lon / 180.0 + 1.0)) as i64;
+                    let goal_y = ((2.0_f64.powf(self.zoom_level as f64 + 7.0)) / PI
+                        * (-lat.to_radians().sin().atanh()
+                            + 85.05112878_f64.to_radians().sin().atanh())) as i64;
+                    let origin=ui.max_rect().min
+                    + (
+                        size / 256.0 * (x % 256) as f32,
+                        size / 256.0 * (y % 256) as f32,
+                    ).into();
+                    let goal = ui.max_rect().min
+                    + (
+                        size / 256.0 * (goal_x - map_x*256) as f32,
+                        size / 256.0 * (goal_y - map_y*256) as f32,
+                    ).into();
+                    painter.line_segment([origin, goal], Stroke::new(1.0, egui::Color32::RED));
+                }
             });
 
             egui::SidePanel::right("GPS_r_panel")
@@ -83,7 +107,11 @@ impl super::AppUI for Gps {
                         ui.add_space(10.0);
                         ui.label(format!("timestamp:\t{}ms", gps_data.timestamp));
                     }
-                    ui.add(egui::Slider::new(&mut self.zoom_level, 10..=13).text("zoom level"));
+
+                    if ui.add(egui::Slider::new(&mut self.zoom_level, 10..=14).text("zoom level")).changed() {
+                        self.pos = None;
+                    }
+
                     if let Some(pos) = self.pos {
 
                         // https://www.trail-note.net/tech/coordinate/
@@ -94,13 +122,17 @@ impl super::AppUI for Gps {
                             * ((- PI * (pos.y as f64 + 256.0 * map_y as f64) / 2.0_f64.powf(self.zoom_level as f64 + 7.0)+85.05112878_f64.to_radians().sin().atanh()).tanh())
                             .asin();
 
-                        ui.label(format!("mouse pos:\t{:1.3},{:1.3}", pos.x, pos.y));
-                        ui.label(format!("mouse pos:\t{:1.3},{:1.3}", pos_lon, pos_lat));
+                        // ui.label(format!("mouse pos:\t{:1.3},{:1.3}", pos.x, pos.y)); // pixel coordinate
+                        ui.label(format!("mouse pos:\t{:1.3},{:1.3}", pos_lon, pos_lat)); // GPS coordinate
+                        if ui.button("set goal").clicked() {
+                            self.goal = Some((pos_lon, pos_lat));
+                        }
                     }
-                    ui.label(format!(
-                        "file://assets/map/{}/{}_{}.png",
-                        self.zoom_level, map_y, map_x
-                    ));
+                    if let Some((lon,lat)) = self.goal {
+                        ui.label(format!("goal:\t{:1.3},{:1.3}", lon, lat));
+                        ui.add_space(20.0);
+                        ui.heading(format!("Distance:\t{:1.3}m", ((self.lat-lat)/0.000008983148616).hypot((self.lon-lon)/0.000010966382364)));
+                    }
                 });
         });
     }
