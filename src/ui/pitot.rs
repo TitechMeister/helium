1f210b02ed3fc851bfec67d6bd161412033c77eb
Yuta Suzuki <suzuki.y.ee@m.titech.ac.jp>
2024-07-19 10:26:48 +0900

    速度表示にCAS(Calibrated Airspeed)を追加

--- src/ui/pitot.rs
+++ src/ui/pitot.rs
@@ -10,7 +10,7 @@ impl PitotUI {
 
 impl super::AppUI for PitotUI {
     fn update(&mut self, data: &mut crate::parse::Parser, ctx: &eframe::egui::Context) {
-        if let Some((pitot_data,_)) = data.get_pitot_data().last() {
+        if let Some((pitot_data, _)) = data.get_pitot_data().last() {
             egui::Window::new("Pitot").vscroll(true).show(ctx, |ui| {
                 ui.heading(format!(
                     "IAS:\t{:2.2}m/s\ttimestamp:\t{}ms",
@@ -19,17 +19,51 @@ impl super::AppUI for PitotUI {
                 egui_plot::Plot::new("velocity")
                     .legend(egui_plot::Legend::default())
                     .show(ui, |plt_ui| {
-                        let point: egui_plot::PlotPoints = data
+                        let point_ias: egui_plot::PlotPoints = data
                             .get_pitot_data()
                             .iter()
                             .enumerate()
-                            .map(|(n, (_data,_))| [n as f64, _data.velocity as f64])
+                            .map(|(n, (_data, _))| [n as f64, _data.velocity as f64])
                             .collect();
-                        let line = egui_plot::Line::new(point)
-                            .color(egui::Color32::from_rgb(255, 0, 0))
-                            .name("IAS")
-                            .fill(0.0);
-                        plt_ui.line(line);
+
+                        let point_cas: egui_plot::PlotPoints = data
+                            .get_pitot_data()
+                            .iter()
+                            .enumerate()
+                            .map(|(n, (pitot_data, pitot_timestamp))| {
+
+                                let c3 = 2.0e-5;
+                                let c2 = 0.0032;
+                                let c1 = 1.0073;
+                                let c0 = 1.0073;
+
+                                let mut c = c0;
+
+                                if let Some((vane_data, _)) = data
+                                    .get_vane_data()
+                                    .iter()
+                                    .rfind(|(_, vane_timestamp)| vane_timestamp <= pitot_timestamp)
+                                {
+                                    c =   c0
+                                        + c1 * vane_data.angle
+                                        + c2 * vane_data.angle * vane_data.angle
+                                        + c3 * vane_data.angle * vane_data.angle * vane_data.angle;
+                                }
+                                [n as f64, (c * pitot_data.velocity) as f64]
+                            })
+                            .collect();
+                        plt_ui.line(
+                            egui_plot::Line::new(point_ias)
+                                .color(egui::Color32::from_rgb(255, 0, 0))
+                                .name("IAS")
+                                .fill(0.0),
+                        );
+                        plt_ui.line(
+                            egui_plot::Line::new(point_cas)
+                                .color(egui::Color32::from_rgb(0, 255, 0))
+                                .name("CAS")
+                                .fill(0.0),
+                        );
                     });
             });
         }
